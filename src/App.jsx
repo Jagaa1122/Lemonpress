7840070287d4563c778789e2e6742a6b73077861
24LP2910 24LP2910 <24LP2910@G304-CC0233.local>
2024-12-17 12:53:30 +0800

    Lemonpress

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./component/Card";
+import Header from "./component/Header";
+import  "./app.css"; 
+
+function App() {
+    return (
+        <div>
+            <Header/>
+           <div className="cards">
+          {
+          
+          }
+           
+            </div>
+        </div>
+    );
+}
+export default App;
